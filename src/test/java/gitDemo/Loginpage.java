c3bc095c02bd213339723a910e79a8c011979e31
Priyanka Sahoo <Priyanka Sahoo10@gmail.com>
2025-03-17 15:22:04 +0530

    login commited

--- src/test/java/gitDemo/Loginpage.java
+++ src/test/java/gitDemo/Loginpage.java
@@ -3,7 +3,11 @@ package gitDemo;
 public class Loginpage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Homepage should be displayed");
+
+	
 
 	}
 
